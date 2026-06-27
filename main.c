80c1324f3ae3b02df70579e4965d69d2d04128b0
Aryan Dahiwal <aryan.dahiwal@gmail.com>
2026-06-27 22:07:13 +0530

    Day 2 - User input for student name, roll, marks

--- main.c
+++ main.c
@@ -2,17 +2,32 @@
 
 struct Student {
     char name[50];
-    int roll;
+    long long roll;  
     float marks;
 };
 
+void printStudent(struct Student s) {
+    printf("Name: %s\n", s.name);
+    printf("Roll: %lld\n", s.roll);  
+    printf("Marks: %.2f\n", s.marks);
+    printf("----------\n");
+}
+
 int main() {
     struct Student s1;
-    s1.roll = 1;
-    s1.marks = 85.5;
     
-    printf("Roll: %d\n", s1.roll);
-    printf("Marks: %.2f\n", s1.marks);
+    printf("Enter name: ");
+    scanf("%s", s1.name);
+    
+    printf("Enter roll number: ");
+    scanf("%lld", &s1.roll);  
+    
+    printf("Enter marks: ");
+    scanf("%f", &s1.marks);
+    
+    printf("\n--- Student Record ---\n");
+    
+    printStudent(s1);
     
     return 0;
 }
